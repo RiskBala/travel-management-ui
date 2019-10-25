462816e5d212e01bc04da50a88a224662b4d724f
P, Balakrishnan <balakrishnan.p@anthem.com>
2019-10-25 20:58:03 +0530

    openshift url

--- src/containers/Home.js
+++ src/containers/Home.js
@@ -19,7 +19,7 @@ export default class Home extends Component {
     componentDidMount() {
         axios({
             method: 'get',
-            url: 'http://localhost:8762/trip-summary/trip',
+            url: 'http://http://zuulserver-s2-travelsystem.apps.na311.openshift.opentlc.com:8762/trip-summary/trip',
             headers: {
                 'Content-Type': 'application/json',
                 'Authorization': 'Bearer ' + this.state.authtoken

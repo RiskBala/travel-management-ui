393bb9501536cb4e196d69c235397f3a02f1eda2
RiskBala <44400207+RiskBala@users.noreply.github.com>
2020-01-03 17:58:11 +0530

    Update Home.js

--- src/containers/Home.js
+++ src/containers/Home.js
@@ -19,7 +19,7 @@ export default class Home extends Component {
     componentDidMount() {
         axios({
             method: 'get',
-            url: 'http://zuulserver-s2-travelsystem.apps.na311.openshift.opentlc.com/trip-summary/trip',
+            url: 'http://zuulserver-s2-travelsystem.apps.na311.openshift.opentlc.com/trip-summary/v1/trip',
             headers: {
                 'Content-Type': 'application/json',
                 'Authorization': 'Bearer ' + this.state.authtoken
